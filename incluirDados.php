c43ed32e1844a29a212337e9c853e2469196d33b
Edison Neves Soares <86014556+edineves@users.noreply.github.com>
2022-01-20 00:08:18 -0300

    add

--- incluirDados.php
+++ incluirDados.php
@@ -12,24 +12,9 @@
 
 		<?php			
 			// Variável
-
-			// Nome dado a um espaço de memória que consegue
-			// armazenar um determinado valor que pode ser alterado (variado)
-			// Exemplo:
-			// $nome="Ana Maria";
-			
-			// Matriz / Vetor
-			// ==============
-			// Nome dado a um espaço de memória que consegue
-			// armazenar diversos valores em posições diferentes que podem
-			// ser alterados (variados)
-			
-			// exemplo:
-			// $nomes[""];
-			
-			// Vetor é o nome dado para uma matriz de 1 dimensão (coluna)
 			
 			// 1. Receber os dados do formulário em variáveis
+
 			$dia		 = $_GET["dia"] ;
 			$localSaida	 = $_GET["localSaida"] ;
 			$dataSaida	 = $_GET["dataSaida"] ;
@@ -44,11 +29,9 @@
             $outrosCustos= $_GET["outrosCustos"] ;
 			$obs		 = $_GET["obs"] ;    
 			
-			// 2. Validar alguns dados básicos
-			/*
-				= Operador de atribuição (permite atribuir um valor p/ a variável)
-				== Operador de comparação (compara o valor da esquerda com o da direita)
-			*/
+			// 2. Validar alguns dados básicos		
+			
+			
 			if ($dia==" ") 
 			{
 				die("O<b>dia</b> deve ser informado. Sistema interrompido.");
@@ -75,11 +58,11 @@
 			{
 				die(" Informe <b>horaChegada</b> válida. Sistema interrompido.");
 			}
-            if ($localchegada==" ")
+            if ($localChegada==" ")
 			{
 				die("Informe <b>localChegada </b> válido. Sistema interrompido.");
 			}
-            if ($kmPercorrido==" ")
+            if ($kmPerc		orrido==" ")
 			{
 				die("Informe<b> kmPercorrido </b> válido. Sistema interrompido.");
 			}
@@ -115,44 +98,20 @@
 			echo "Distância percorrida: <b>$kmPercorrido</b><br>";
 			echo "valor pago em gasolina: <b>$gasolina</b><br>";
 			echo "valor pago com pedágio: <b>$pedagio</b><hr>";
-            echo "valor pago com refeições: <b>$reficoes</b><br>";
+            echo "valor pago com refeições: <b>$refeicoes</b><br>";
 			echo "Outros custos no trecho: <b>$outrosCustos</b><br>";
 			echo "Observações quanto ao percurso: <b>$obs</b><br>";
 		
 			
 			// 4 - Abrindo o banco de dados
-			// .1- Conexão com o servidor
-			
-			// mysqli_connect( p1, p2, p3)
-			
-			// Parâmetros do tipo string
-			// p1 - endereço do servidor MYSQL
-			// p2 - nome do usuário existente no MYSQL
-			// p3 - senha do usuário existente no MYSQL
-			//
-			// retorna um objeto de conexão 
-			// que nos permite, posteriormente, enviar comandos
-			// para este MYSQL
-			
-			// As funções do PHP para manipulação do banco de dados MYSQL
-			// iniciam com o prefixo mysqli_
+			// .1- Conexão com o servidor	
 
-			// Funções mais usadas:
-			// mysqli_connect()     - Conecta no servidor mysql
-			// mysqli_select_db()   - Abre um banco de dados MYSQL
-			// mysqli_error()		- Traz o último erro ocorrido no MYSQL
-			// mysqli_query() 	    - Envia um comando SQL para o MYSQL 
-			// mysqli_fetch_array() - Cria uma matriz com base num record set_error_handler
-			// mysqli_num_rows()	- Conta quantos registros um record set tem
+		
 			
 			$con = mysqli_connect("localhost","root", "");
 			
 			// .2 - Abertura do banco de dados
-			
-			// mysqli_select_db(p1, p2)
-			// Parâmetros do tipo string
-			// p1 - objeto de conexão existente
-			// p2 - nome do banco de dados (string)
+
 			
 			mysqli_select_db($con, "travel") or 
 				die(
@@ -185,33 +144,23 @@
                         '$dataChegada', 
 						'$horaChegada',  
 						'$localChegada',  
-						'$KmPercorrido',
+						'$kmPercorrido',
                         '$gasolina',  
 						'$pedagio',  
-						'$reficoes',
+						'$refeicoes',
                         '$outrosCustos',                 
-						 $obs )";
+						'$obs' )";
 			
 			// Visualizar o comando e testar p/ ver se não está com erros
-			// Mostro na tela e copio e colo no console do MYSQL
-			// Comando estando ok, paro de mostrar na tela 
 			
-			// echo $sql;
-			
-			// 2.Enviando o comando de inclusão para o banco
-			
-			// mysqli_query(p1, p2)
-			// Parâmetros do tipo 
-			// p1 - objeto de conexão existente
-			// p2 - comando SQL a ser executado (string ou variável)
 			
 			mysqli_query($con, $sql) or 
 				die(
-					"Houve um problema na inserção do Pet:<br>" . 
+					"Houve um problema para cadastrar os dados:<br>" . 
 						mysqli_error($con)
 				);
 			
-			echo "travelDates <b>$dia</b> incluído com sucesso!";
+			echo "travelDates <b >$dia </b> incluído com sucesso!";
 			
 		?>
 		
